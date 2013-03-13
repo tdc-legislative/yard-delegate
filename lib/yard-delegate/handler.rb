e9b91d7e2233a0e3ebf951e39aeadf720f058f9b
Ryan Taylor Long <ryan@rtlong.com>
2013-03-13 17:37:21 -0500

    Add support for allow_nil and prefix options

--- lib/yard-delegate/handler.rb
+++ lib/yard-delegate/handler.rb
@@ -1,24 +1,72 @@
 require 'yard'
-
+require 'pry'
 module YARD
   module Delegate
     class Handler < YARD::Handlers::Ruby::Base
 
       handles method_call(:delegate)
 
-      def delegate(*names)
-        hash = names.pop
-        to   = hash[:to] || hash["to"]
+      def delegate(*methods)
+        options = methods.pop
+        to = options[:to]
 
-        names.each do |name|
-          register YARD::CodeObjects::MethodObject.new(namespace, name, scope) { |o|
-            o.parameters = [["*args", nil], ["&block", nil]]
-            o.source = "def #{name}(*args, &block)\n  #{to}.#{name}(*args, &block)\nend"
-            o.signature = "def #{name}(*args, &block)"
-            o.docstring = statement.comments.to_s.empty? ? "" : statement.comments
+        prefix, allow_nil = options.values_at(:prefix, :allow_nil)
+
+
+        method_prefix = \
+          if prefix
+            "#{prefix == true ? to : prefix}_"
+          else
+            ''
+          end
+
+        to = to.to_s
+        to = 'self.class' if to == 'class'
+
+        methods.each do |method|
+          # Attribute writer methods only accept one argument. Makes sure []=
+          # methods still accept two arguments.
+          args = (method =~ /[^\]]=$/) ? %w[ arg ] : %w[ *args &block ]
+          definition = args.join(', ')
+          signature = "def #{method_prefix}#{method}(#{definition})"
+
+          if allow_nil
+            source = <<-EOS
+              #{signature}
+                if #{to} || #{to}.respond_to?(:#{method})
+                  #{to}.#{method}(#{definition})
+                end
+              end
+            EOS
+          else
+            exception = %(raise "\#{self.class}##{method_prefix}#{method} delegated to #{to}.#{method}, but #{to} is nil: \#{self.inspect}")
+
+            source = <<-EOS
+              #{signature}
+                #{to}.#{method}(#{definition})
+              rescue NoMethodError
+                if #{to}.nil?
+                  #{exception}
+                else
+                  raise
+                end
+              end
+            EOS
+          end
+
+          register YARD::CodeObjects::MethodObject.new(namespace, "#{method_prefix}#{method}", scope) { |o|
+            o.parameters = args.map{|a| [a, nil] }
+            o.signature = signature
+            o.source = source
+            comment = statement.comments.to_s.empty? ? "Delegates to #{to}.#{method}" : statement.comments
+            comment << "\n@see ##{to}"
+            o.docstring = comment
             o.delegate = to
           }
         end
+      rescue => ex
+        p ex
+        raise
       end
 
       process do
